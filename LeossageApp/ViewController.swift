84e053a6a7fb66d090e83469b90bba9db55386d4
Emre Celebi <emrecelebiiii@gmail.com>
2019-08-30 19:11:25 +0300

    First Commit

---
+++ LeossageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeossageApp
+//
+//  Created by Yunus Emre Celebi on 30.08.2019.
+//  Copyright © 2019 CLevel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
